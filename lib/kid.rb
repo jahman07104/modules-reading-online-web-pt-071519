f2febf051d971fd06914828f80771d8300c44e7d
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 13:24:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -9,9 +9,7 @@ class Kid
   # def initialize(name)
   #   @name = name
   # end
-   extend 
-    FancyDance::ClassMethods
-   include 
-   FancyDance::InstanceMethods
+   extend FancyDance::ClassMethods
+   include FancyDance::InstanceMethods
 
 end
