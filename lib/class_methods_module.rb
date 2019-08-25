0afcbe07c3ce5aba2dfdcc1e2aef4882fb051ca5
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 12:52:29 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -4,11 +4,3 @@
 #     "This class produces objects that love to dance."
 #   end
 # end
-
- module ClassMethods
- 
-    def metadata
-      "This class produces objects that love to dance."
-    end
-  end
-end
