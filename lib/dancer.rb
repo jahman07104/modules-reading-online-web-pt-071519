83688b0e730665aecaff5ceaca9ca4c5ba1ad901
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 13:04:29 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,12 +3,12 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
   
-  include Dance
-  attr_accessor :name
+  # include Dance
+  # attr_accessor :name
  
-  def initialize(name)
-    @name = name
-  end
+  # def initialize(name)
+  #   @name = name
+  # end
   
    extend 
    FancyDance::ClassMethods
