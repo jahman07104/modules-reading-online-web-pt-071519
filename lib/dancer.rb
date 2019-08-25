9c3da4f9f449cf2a41d133e8c2577ca25587d967
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 13:29:52 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,7 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  
-
-  attr_accessor :name
+   attr_accessor :name
  
   def initialize(name)
     @name = name
