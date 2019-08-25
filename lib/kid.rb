eb25761d1e3795016586fbb6a5cceb8af873d563
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 13:27:49 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
   
-  # include Dance
+   include Dance
   # attr_accessor :name
  
   # def initialize(name)
