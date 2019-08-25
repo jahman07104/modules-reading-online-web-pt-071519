744e0e154959545f29bd9536170771f9c6115389
Patrick Harrison <jahman@bellatlantic.net>
2019-08-25 12:38:58 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,6 +2,8 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
+  
+  include Dance
    extend MetaDancing
   # FancyDance::ClassMethods
   # include 
